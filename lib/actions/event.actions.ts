5baf34afca0a26bea1bc337f7405f968af450f40
emredkyc <emredkyc@hotmail.com>
2023-12-24 19:55:22 +0100

    format documents

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -32,8 +32,9 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
   try {
     await connectToDatabase()
 
-    const organizer = await User.findById(userId)
-    if (!organizer) throw new Error('Organizer not found')
+    const organizer = await User.findById(userId);
+
+    if (!organizer) throw new Error('Organizer not found');
 
     const newEvent = await Event.create({ ...event, category: event.categoryId, organizer: userId })
     revalidatePath(path)
