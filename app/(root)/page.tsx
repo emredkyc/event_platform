0f17ef9c022aac1bdffff413bb5513c671c31c6a
emredkyc <emredkyc@hotmail.com>
2023-12-26 19:20:59 +0100

    add CategoryFilter component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from '@/components/ui/button'
+import CategoryFilter from '@/components/shared/CategoryFilter';
 import Collection from '@/components/shared/Collection'
-import Image from 'next/image'
-import Link from 'next/link'
+import Search from '@/components/shared/Search';
+import { Button } from '@/components/ui/button'
 import { getAllEvents } from '@/lib/actions/event.actions';
 import { SearchParamProps } from '@/types';
-import Search from '@/components/shared/Search';
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default async function Home({ searchParams }: SearchParamProps) {
   const page = Number(searchParams?.page) || 1;
@@ -17,44 +18,40 @@ export default async function Home({ searchParams }: SearchParamProps) {
     page,
     limit: 6
   })
+
   return (
     <>
-      <section className="bg-primary-50 bg-dotted-patern bg-contain py-5 md:py-10">
-        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2x1:gap-0">
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Host, Connect, Celebrate: Your 
-            Events, Our Platform!</h1>
-            <p className="p-regular-20 md:p-regular-24">Book and learn
-            helpful tips from 3,168+ mentors in world-class companies
-            with our global community.
-            </p>
-            <Button size="lg" asChild className="button w-full
-            sm:w-fit">
+            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
+            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">
                 Explore Now
               </Link>
             </Button>
           </div>
-          <Image
+
+          <Image 
             src="/assets/images/hero.png"
             alt="hero"
             width={1000}
             height={1000}
-            className="max-h-[70vh] object-contain object-center 2
-            2x1:max-h-[50vh]"
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
           />
         </div>
-      </section>
-      <section id="events" className="wrapper my-8 flex flex-col gap-8md:gap-12">
-        <h2 className="h2-bold">Trust by <br/> Thousands of Events
-        </h2>
+      </section> 
 
-        <div className="flex- w-full flex-col gap-5 md:flex-row">
-          <Search/>
-          CategoryFilter
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          <Search />
+          <CategoryFilter />
         </div>
 
-        <Collection
+        <Collection 
           data={events?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
